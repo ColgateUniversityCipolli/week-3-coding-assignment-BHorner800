b3b8e34f18cbb7892cbf5bb46a5c98610faf6995
BHorner800 <benjamin.r.horner@gmail.com>
2025-02-11 11:48:00 -0500

    HW3 Code Finished
    In code file

--- HW3_Code.R
+++ HW3_Code.R
@@ -6,7 +6,30 @@
 #Problem 1
 #########
 
-#All integers are the product of unique prime numbers
+#Libraries to load
+library(primes)
+
+#generating a prime number list with a max of 95 because we want 
+#their products that are less than 100
+prime.nums = generate_primes(max = 100)
+
+#list to be filled in later
+product.of.primes = c() 
+
+
+#looping through the products of all the primes we generated
+for (num in prime.nums){
+  #list of prime products with num
+  prime.products = num*prime.nums
+  #gets rid of square of num
+  prime.products = prime.products[which(prime.products != num*num)]
+  #Keep only products <= 100
+  prime.products = prime.products[which(prime.products <= 100)]
+  #combines them into a list of the unique prime products less than 100
+  #sorted to ascending numeric value for ease of viewing
+  product.of.primes = sort(union(product.of.primes, prime.products))
+}
+
 
 blake.list = c(6, 10, 14, 15, 21, 
                22, 26, 33, 34, 35, 
@@ -14,3 +37,10 @@ blake.list = c(6, 10, 14, 15, 21,
                57, 58, 62, 65, 69,
                75, 77, 82, 85, 86,
                87, 91, 93, 94, 95)
+
+
+wrong.num = setdiff(blake.list, product.of.primes)
+correction = setdiff(product.of.primes, blake.list)
+
+paste("The wrong number in Blake's list is ", toString(wrong.num), 
+      ". The correct number is ", toString(correction), ".", sep = "")
